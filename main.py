700f624db94dabb3ccf5d21be9669aeee1736a23
Varen Kumar <varenkumar15@gmail.com>
2026-09-18 23:55:14 +0530

    Updated main and implemented fastapi

--- main.py
+++ main.py
@@ -1,41 +1,67 @@
 from src.github_client import GitHubClient
 import pandas as pd
+from fastapi import FastAPI
+app=FastAPI()
 
-client = GitHubClient()
+def build_stats():
+        client = GitHubClient()
 
-repos = client.get_repos()
-all_data=[]
-for repo in repos:
-    repo_name = repo["name"]
-    commits=client.get_commits(repo_name)
-    languages=client.get_languages(repo_name)
-    repo_data = {
-    "name": repo_name,
-    "stars": repo["stargazers_count"],
-    "forks": repo["forks_count"],
-    "language": repo["language"],
-    "url": repo["html_url"],
-    "commit_count": len(commits),
-    "languages":languages
+        repos = client.get_repos()
+        all_data=[]
+        for repo in repos:
+            repo_name = repo["name"]
+            commits=client.get_commits(repo_name)
+            languages=client.get_languages(repo_name)
+            repo_data = {
+            "name": repo_name,
+            "stars": repo["stargazers_count"],
+            "forks": repo["forks_count"],
+            "language": repo["language"],
+            "url": repo["html_url"],
+            "commit_count": len(commits),
+            "languages":languages
+            }
+            all_data.append(repo_data)
+
+
+        df=pd.DataFrame(all_data)
+        return df
+@app.get("/")
+def root():
+    return {"message": "GitStat API is running"}
+@app.get("/stats")
+def get_stats():
+    df = build_stats()
+
+    return {
+        "total_repositories": len(df),
+        "total_commits": int(df["commit_count"].sum()),
+        "average_commits_per_repository": float(
+            df["commit_count"].mean()
+        )
     }
-    all_data.append(repo_data)
 
 
-df=pd.DataFrame(all_data)
 
-df.to_csv("data/github_stats.csv", index=False)
-print(df)
-print("\n===== GITHUB ANALYTICS =====")
 
-print(f"Total repositories: {len(df)}")
 
-print(f"Total commits: {df['commit_count'].sum()}")
 
-print(f"Average commits per repository: {df['commit_count'].mean():.2f}")
 
-most_active = df.loc[df["commit_count"].idxmax()]
 
-print(f"Most active repository: {most_active['name']}")
 
-print("\nPrimary languages:")
-print(df["language"].value_counts())
+# df.to_csv("data/github_stats.csv", index=False)
+# print(df)
+# print("\n===== GITHUB ANALYTICS =====")
+
+# print(f"Total repositories: {len(df)}")
+
+# print(f"Total commits: {df['commit_count'].sum()}")
+
+# print(f"Average commits per repository: {df['commit_count'].mean():.2f}")
+
+# most_active = df.loc[df["commit_count"].idxmax()]
+
+# print(f"Most active repository: {most_active['name']}")
+
+# print("\nPrimary languages:")
+# print(df["language"].value_counts())
